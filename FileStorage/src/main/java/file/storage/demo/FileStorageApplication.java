bd9a68c18fa948669da258582d48e3e9d59a14ec
Đặng Đạt <73807126+datdt198213@users.noreply.github.com>
2022-05-18 23:40:39 +0700

    da chay duoc trong intelij

--- FileStorage/src/main/java/file/storage/demo/FileStorageApplication.java
+++ FileStorage/src/main/java/file/storage/demo/FileStorageApplication.java
@@ -2,8 +2,12 @@ package file.storage.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"file.storage.demo"})
+@EnableConfigurationProperties({
+		FileStorageProperties.class
+})
 public class FileStorageApplication {
 
 	public static void main(String[] args) {
